dc83c2d125c9035a5a664e2cb53c273ea8fbe054
wendel Anchieta <wendel.anchieta@valec.gov.br>
2019-01-21 17:27:04 -0200

    Lazy loading

---
+++ sioca-ionic-frontend/src/pages/home/home.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { HomePage } from './home';
+import { IonicPageModule } from 'ionic-angular/module';
+
+@NgModule({
+  declarations: [HomePage],
+  imports: [IonicPageModule.forChild(HomePage)]
+})
+export class HomeModule {
+  
+}
