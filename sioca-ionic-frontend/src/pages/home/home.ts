481126dc757537747a3416caef6ec42d02dd278e
wendel Anchieta <wendel.anchieta@valec.gov.br>
2019-01-21 18:55:13 -0200

    Desabilitar menu na tela inicial

--- sioca-ionic-frontend/src/pages/home/home.ts
+++ sioca-ionic-frontend/src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -8,12 +8,22 @@ import { NavController, IonicPage } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public menu: MenuController) {
 
   }
 
   login() {
     this.navCtrl.setRoot('OcorrenciasPage');
+  }
+
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+  
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
   }
 
 }
