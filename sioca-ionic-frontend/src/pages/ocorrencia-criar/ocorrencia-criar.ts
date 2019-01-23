39c5cdfec840a19668b3a9199a76a8a807c23fdd
wendel Anchieta <wendel.anchieta@valec.gov.br>
2019-01-23 19:00:31 -0200

    Criando um FormGroup para controlar o formulario

--- sioca-ionic-frontend/src/pages/ocorrencia-criar/ocorrencia-criar.ts
+++ sioca-ionic-frontend/src/pages/ocorrencia-criar/ocorrencia-criar.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the OcorrenciaCriarPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -15,7 +9,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class OcorrenciaCriarPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup;
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public formBuilder: FormBuilder) {
+
+      this.formGroup = this.formBuilder.group({
+        //codigoOcorrencia: ['',[Validators.required, Validators.min(5), Validators.maxLength(120)]],
+        codigoOcorrencia: ['',[]],
+        trechoId: [null,[]],
+        loteId: [null,[]],
+        kmInicial: ['',[]],
+        kmFinal: ['',[]],
+        dataRegistro: ['',[]],
+        localizacaoId: [null,[]],
+        tipoOcorrenciaId: [null,[]],
+        gravidade: [null,[]],
+        situacao: [null,[]],
+        topicoPBAId: [null,[]],
+        tipoLado: [null,[]],
+        descricao: ['',[]]
+      });
   }
 
   ionViewDidLoad() {
