6da821abf4f7201fd1a6f8b2aeb5004307f7b290
wendel Anchieta <wendel.anchieta@valec.gov.br>
2019-01-29 17:05:13 -0200

    Ajustando a funcionalidade salvar usuario

--- sioca-ionic-frontend/src/pages/ocorrencia-criar/ocorrencia-criar.ts
+++ sioca-ionic-frontend/src/pages/ocorrencia-criar/ocorrencia-criar.ts
@@ -53,13 +53,14 @@ export class OcorrenciaCriarPage {
       loteId: [null, []],
       kmInicial: ['', []],
       kmFinal: ['', []],
-      dataRegistro: ['', []],
+      dataRegistro: [new Date(), []],
       localizacaoId: [null, []],
       tipoOcorrenciaId: [null, []],
-      gravidade: [null, []],
+      gravidade: ['', []],
       situacao: [null, []],
       topicoPBAId: [null, []],
       tipoLado: [null, []],
+      emergencial: ['', []],
       descricao: ['', []]
     });
   }
@@ -105,15 +106,15 @@ export class OcorrenciaCriarPage {
     let oc = <OcorrenciaDTO>{}
     oc.lote = <LoteDTO>{};
     oc.lote.trecho = <TrechoDTO>{};
-    //oc.localizacao = <LocalizacaoDTO>{};
-    //oc.tipoOcorrencia = <TipoOcorrenciaDTO>{}
-    //oc.topicoPBA = <TopicopbaDTO>{};
+    oc.localizacao = <LocalizacaoDTO>{};
+    oc.tipoOcorrencia = <TipoOcorrenciaDTO>{}
+    oc.topicoPBA = <TopicopbaDTO>{};
 
 
     oc.codigoOcorrencia = this.formGroup.controls.codigoOcorrencia.value;
     oc.lote.trecho.id = this.formGroup.controls.trechoId.value;
     oc.lote.id = this.formGroup.controls.loteId.value;
-    /*oc.kmInicial = this.formGroup.controls.kmInicial.value;
+    oc.kmInicial = this.formGroup.controls.kmInicial.value;
     oc.kmFinal = this.formGroup.controls.kmFinal.value;
     oc.dataRegistro = this.formGroup.controls.dataRegistro.value;
     oc.localizacao.id = this.formGroup.controls.localizacaoId.value;
@@ -122,8 +123,9 @@ export class OcorrenciaCriarPage {
     oc.situacao = this.formGroup.controls.situacao.value;
     oc.topicoPBA.id = this.formGroup.controls.topicoPBAId.value;
     oc.tipoLado = this.formGroup.controls.tipoLado.value;
-    oc.descricao = this.formGroup.controls.descricao.value;*/
-    
+    oc.emergencial = this.formGroup.controls.emergencial.value;
+    oc.descricao = this.formGroup.controls.descricao.value;
+        
     this.ocorrencia = oc;
   }
 
